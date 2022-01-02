d919ce95b7cb67f8f83e32172b6fbeaa37b4837b
Paul Goossens <pgoossens@move4mobile.com>
2022-01-02 13:46:55 +0100

    get data

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { LanguageModel } from '../models/language.model';
+import { TranslationModel } from '../models/translation.model';
+import { LanguagesService } from '../services/languages.service';
+import { TranslationsService } from '../services/translations.service';
+
+export interface Data {
+  language: LanguageModel;
+  translations: TranslationModel[];
+}
 
 @Component({
   selector: 'app-index',
@@ -6,7 +16,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
-  constructor() {}
+  data: Data[];
 
-  ngOnInit(): void {}
+  constructor(
+    private languageService: LanguagesService,
+    private translationService: TranslationsService
+  ) {
+    this.data = [];
+  }
+
+  ngOnInit(): void {
+    this.initLanguages();
+  }
+
+  private initLanguages(): void {
+    this.languageService
+      .getLanguages()
+      .pipe(
+        tap(languages => {
+          languages.forEach(language => {
+            this.initTranslations(language);
+          });
+        })
+      )
+      .subscribe();
+  }
+
+  private initTranslations(language: LanguageModel) {
+    this.translationService
+      .getTranslations(language.guid)
+      .pipe(
+        tap(translations => {
+          this.initData(language, translations);
+        })
+      )
+      .subscribe();
+  }
+
+  private initData(language: LanguageModel, translations: TranslationModel[]) {
+    this.data.push({
+      language,
+      translations,
+    });
+  }
 }
